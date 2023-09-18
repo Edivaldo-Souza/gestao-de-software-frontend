43362d89deec3602fb772555c24c9da4321d2b83
Edivaldo <edivaldoraimundo84@gmail.com>
2023-09-18 09:04:35 -0300

    primeiro commit

---
+++ src/telaLogin/index.js
@@ -0,0 +1,18 @@
+
+
+function Login(){
+    return(
+       <div id="container">
+        <div class="form">
+            <h1>Gestor de Softwares</h1>
+            <input type="text" name="nome"></input>
+            <input type="text" name="senha"></input>
+        </div>
+        <div class="image">
+
+        </div>
+       </div> 
+    )
+}
+
+export default Login;
